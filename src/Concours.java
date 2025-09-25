100be6c76e550564ee70e025dd41514ea5b5c6a6
YuriAyato144 <aedan.belet@divtec.ch>
2025-09-25 12:51:42 +0200

    vaiables dans certaines calsses

--- src/Concours.java
+++ src/Concours.java
@@ -1,2 +1,6 @@
+import java.util.ArrayList;
 public class Concours {
+    String nomConcours;
+    String lieuxConcours;
+    int capacité;
 }
--- src/Employe.java
+++ src/Employe.java
@@ -1,2 +1,3 @@
+import java.util.ArrayList;
 public class Employe {
 }
